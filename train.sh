7d01054427cc0b7bf4f569b023426ccbe5e0f073
Gooogr <gusarovga@ya.ru>
2023-03-31 16:28:28 +0000

    fix: correct typo loss -> eval_loss

--- train.sh
+++ train.sh
@@ -9,7 +9,7 @@ python3 ./src/train.py \
   --weight_decay 0.01 \
   --logging_steps 1000 \
   --load_best_model_at_end True \
-  --metric_for_best_model f1 \
+  --metric_for_best_model eval_loss \
   --greater_is_better True \
   --do_train True \
   --do_eval True \
