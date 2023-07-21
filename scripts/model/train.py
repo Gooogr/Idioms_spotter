7a1e350744121a7a4bca34ac72c6e2b7a311130e
grigoriy <gusarov.sys@gmail.com>
2023-07-21 12:06:28 +0300

    fix: code linting

--- scripts/model/train.py
+++ scripts/model/train.py
@@ -1,4 +1,4 @@
-# pylint: disable=C0103,W0632
+# pylint: disable=C0103,W0632,W0621,E1102
 
 """
 Fine-tuning HF models for PIEs token classification.
@@ -65,7 +65,9 @@ class CustomTrainer(Trainer):
         outputs = model(**inputs)
         logits = outputs.get("logits")
         # compute custom loss
-        loss_fct = torch.nn.CrossEntropyLoss(weight=torch.tensor(self.class_weights)).to(self.device)
+        loss_fct = torch.nn.CrossEntropyLoss(
+            weight=torch.tensor(self.class_weights)
+        ).to(self.device)
         loss = loss_fct(logits.view(-1, self.model.config.num_labels), labels.view(-1))
         return (loss, outputs) if return_outputs else loss
 
